54b7341e21a7052805e0c574c9d6ff3de758ff99
Crim, Austin <Crim.Austin@principal.com>
2020-07-16 13:14:24 -0500

    remove next-auth

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,18 +1,22 @@
-export default function Button(props) {
-    const { buttonStyle } = props;
+import { on } from 'process';
+
+export default function Button({ buttonStyle, onClick = undefined, children }) {
     let classList = 'rounded transition-all duration-75 ease-in ';
 
     switch (buttonStyle) {
         case 'blue':
-            classList += 'px-4 py-2 text-gray-100 bg-blue-400 hover:bg-blue-500';
+            classList +=
+                'px-4 py-2 text-gray-100 bg-blue-400 hover:bg-blue-500';
             break;
         case 'red':
             classList += 'px-4 py-2 text-red-700 bg-red-200 hover:bg-red-300';
             break;
         case 'link':
-            classList += 'text-blue-400 hover:text-blue-600';
+            classList += 'text-blue-400 hover:text-blue-600 whitespace-no-wrap';
             break;
     }
 
-    return <button className={classList} {...props}></button>;
+    return <button className={classList} onClick={onClick}>
+        {children}
+    </button>;
 }
