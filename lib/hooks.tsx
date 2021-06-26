f165aff2e3cac3aa7b66981f7ad64acecfbafa63
Austin Crim <crim.austin@principal.com>
2021-06-25 21:07:31 -0500

    fix build

--- lib/hooks.tsx
+++ lib/hooks.tsx
@@ -1,8 +1,9 @@
+import { Tweet } from '@prisma/client'
 import { useQuery } from 'react-query'
 import { fetcher } from './fetcher'
 
 export function useFeed() {
-  const { status, data, error } = useQuery('/api/feed', () =>
+  const { status, data, error } = useQuery<Tweet[], Error>('/api/feed', () =>
     fetcher('/api/feed')
   )
   return { data, status, error }
